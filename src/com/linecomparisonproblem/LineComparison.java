efef460e71b76d9c54056e52d4749f743fdf91db
amitranjan <amitranjanpradhan456@gmail.com>
2021-05-14 12:31:16 +0530

    [Amit] Added Welcome Message

---
+++ src/com/linecomparisonproblem/LineComparison.java
@@ -0,0 +1,7 @@
+package com.linecomparisonproblem;
+
+public class LineComparison {
+	public static void main(String[] args) {
+		System.out.println("Welcome To Line Comparison Computation Program");
+	}
+}
